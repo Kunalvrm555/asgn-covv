4cfbb508ada3552e669c42478fc8c657889d0c62
Kunal Verma <kunalvrm555@gmail.com>
2023-05-10 21:07:18 +0530

    pg to sequelize for db operations

--- index.js
+++ index.js
@@ -1,52 +1,66 @@
 const express = require('express');
 require('dotenv').config();
 const bodyParser = require('body-parser');
-const { Pool } = require('pg');
+const { Sequelize, Op } = require('sequelize');
+
 const app = express();
 const port = 3000;
 app.use(bodyParser.json());
-const pool = new Pool({
-    //fetch user and db details from .env file
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: 5432,
+
+// create sequelize instance
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
 });
 
-//test the database connection
-pool.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database', err);
-    } else {
-        console.log('Connected to the database');
-    }
+// test the database connection
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connected to the database');
+  } catch (err) {
+    console.error('Error connecting to the database', err);
+  }
+})();
+
+// create Product model
+const Product = sequelize.define('Product', {
+  name: Sequelize.STRING,
+  category: Sequelize.STRING,
+  price: Sequelize.INTEGER
 });
+
+// synchronize the model with the database
+(async () => {
+  await sequelize.sync();
+  console.log('Product model synchronized with the database');
+})();
 
 app.get('/products', async (req, res) => {
-    try {
-        const { category } = req.query;
-        let query = 'SELECT * FROM products';
+  try {
+    const { category } = req.query;
+    const options = category ? { where: { category } } : {};
 
-        if (category) {
-            query = `SELECT * FROM products WHERE category = '${category}'`;
-        }
-
-        const result = await pool.query(query);
-        res.json(result.rows);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server Error');
-    }
+    const products = await Product.findAll(options);
+    res.json(products);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
 });
+
 app.get('/categories', async (req, res) => {
-    try {
-        const { rows } = await pool.query('SELECT DISTINCT category FROM products');
-        res.send(rows.map(row => row.category));
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server error');
-    }
+  try {
+    const categories = await Product.findAll({
+      attributes: [
+        [Sequelize.fn('DISTINCT', Sequelize.col('category')), 'category']
+      ]
+    });
+    res.send(categories.map(c => c.category));
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
 });
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
--- script.js
+++ script.js
@@ -1,73 +1,50 @@
-const { Pool } = require('pg');
+const { Sequelize, DataTypes } = require('sequelize');
 const fs = require('fs');
 require('dotenv').config();
 
-const pool = new Pool({
-    //fetch user and db details from .env file
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: 5432,
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres'
 });
 
-const createTableQuery = `
-  CREATE TABLE IF NOT EXISTS products (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    category VARCHAR(255) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL
-  );
-`;
+const Product = sequelize.define('Product', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  price: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false
+  }
+});
 
-async function createTable() {
-    const client = await pool.connect();
-
-    try {
-        // Create the table if it doesn't exist
-        await pool.query(createTableQuery);
-
-        await client.query('COMMIT');
-
-        console.log('Table created successfully!');
-    } catch (err) {
-        await client.query('ROLLBACK');
-        console.error(err);
-    } finally {
-        client.release();
-    }
+async function syncDatabase() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connected to the database');
+    await Product.sync({ force: true });
+    console.log('Product model synchronized with the database');
+  } catch (err) {
+    console.error('Unable to connect to the database:', err);
+  }
 }
 
 async function insertProducts() {
-    const client = await pool.connect();
-
-    try {
-        await client.query('BEGIN');
-
-        const products = JSON.parse(fs.readFileSync('products.json', 'utf8'));
-
-        // Insert all the products into the table
-        for (const product of products) {
-            const { name, category, price } = product;
-            const query = {
-                text: 'INSERT INTO products(name, category, price) VALUES($1, $2, $3)',
-                values: [name, category, price],
-            };
-            await client.query(query);
-        }
-
-        await client.query('COMMIT');
-        console.log('Products inserted successfully!');
-    } catch (err) {
-        await client.query('ROLLBACK');
-        console.error(err);
-    } finally {
-        client.release();
-    }
+  try {
+    const products = JSON.parse(fs.readFileSync('products.json', 'utf8'));
+    await Product.bulkCreate(products);
+    console.log('Products inserted successfully!');
+  } catch (err) {
+    console.error('Error inserting products:', err);
+  } finally {
+    await sequelize.close();
+  }
 }
 
-createTable().then(() => {
-    insertProducts().then(() => {
-        pool.end();
-    });
+syncDatabase().then(() => {
+  insertProducts();
 });
