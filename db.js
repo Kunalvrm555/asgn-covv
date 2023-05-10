2c961d1cbc0766b1e8856f2b573f28fa44be1cc9
Kunal Verma <kunalvrm555@gmail.com>
2023-05-10 22:49:44 +0530

    optimize index.js and modularize

---
+++ db.js
@@ -0,0 +1,23 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+});
+
+const testConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connected to the database');
+  } catch (err) {
+    console.error('Error connecting to the database', err);
+  }
+};
+
+const syncModels = async () => {
+  await sequelize.sync();
+  console.log('models synchronized with the database');
+};
+
+module.exports = { sequelize, testConnection, syncModels };
--- index.js
+++ index.js
@@ -1,44 +1,18 @@
 const express = require('express');
 require('dotenv').config();
 const bodyParser = require('body-parser');
-const { Sequelize } = require('sequelize');
-
+const { sequelize, Product } = require('./models');
+const { testConnection, syncModels } = require('./db');
 const app = express();
 const port = 3000;
 app.use(bodyParser.json());
 
-// create sequelize instance
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
-});
-
-// test the database connection
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Connected to the database');
-  } catch (err) {
-    console.error('Error connecting to the database', err);
-  }
-})();
-
-// create Product model
-const Product = sequelize.define('Product', {
-  name: Sequelize.STRING,
-  category: Sequelize.STRING,
-  price: Sequelize.INTEGER
-});
-
-// synchronize the model with the database
-(async () => {
-  await sequelize.sync();
-  console.log('Product model synchronized with the database');
-})();
+testConnection(); // test connection to database
+syncModels(); // sync models to database
 
 app.get('/products', async (req, res) => {
   try {
-    const { category, page = 1, limit = 5} = req.query;
+    const { category, page = 1, limit = 5 } = req.query;
     const options = category ? { where: { category } } : {};
     const offset = (page - 1) * limit;
 
@@ -54,12 +28,11 @@ app.get('/products', async (req, res) => {
   }
 });
 
-
 app.get('/categories', async (req, res) => {
   try {
     const categories = await Product.findAll({
       attributes: [
-        [Sequelize.fn('DISTINCT', Sequelize.col('category')), 'category']
+        [sequelize.fn('DISTINCT', sequelize.col('category')), 'category']
       ]
     });
     res.send(categories.map(c => c.category));
